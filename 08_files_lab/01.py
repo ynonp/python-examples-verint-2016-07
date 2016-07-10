7ecd83de5ed593f7627338fa885021af9d73028e
ynonp <ynonperek@gmail.com>
2016-07-10 22:35:30 +0300

    Update 01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,6 @@
 """
 Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
+and appends the first file's contents to
+the end of the second one
 """
 
